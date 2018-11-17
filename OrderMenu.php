892f4b22ca0aa83cdbca57b443a7edfb05121d51
Pete <anhbaokhanh@hotmail.com>
2018-11-16 17:03:52 -0800

    Main menu
    Order menu for pizza and side orders

---
+++ OrderMenu.php
@@ -0,0 +1,56 @@
+
+<html>
+
+<head><h1>
+Fleetwood Pizza
+</h1>
+</head>
+
+<body>
+<h2>
+Place your order
+</h2>
+<main>
+    <aside>
+        <h1>Pizza</h1>
+        <nav>
+        <ul>
+            <!-- display links for all categories -->
+                        <li>
+                <a href="?category_id=1">
+                    Pepperoni Pizza               </a>
+            </li>
+                        <li>
+                <a href="?category_id=2">
+                    Triple Cheese Pizza                </a>
+            </li>
+                        <li>
+                <a href="?category_id=3">
+                    Hawaiian Pizza                </a>
+            </li>
+                    </ul>
+        </nav>
+    </aside>
+    <section>
+        <h1>Side Orders</h1>
+        <nav>
+        <ul>
+            <!-- display links for products in selected category -->
+                        <li>
+						 <a href="?category_id=3">
+						Lasagna			</a>
+            </li>
+                    </ul>
+                
+        </nav>
+    </section>
+</main>
+
+
+
+</body>
+
+
+
+
+</html>
---
+++ Sub Menu.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+Sub-MENU
+</head>
+<body>
+
+
+
+
+
+< h1 > Choose Quantity< /h1 >
+< div >
+< a href=”” > HOME < /a >
+< a href=”OrderMenu.html” > MENU < /a >
+< a href=”” > CART < /a >
+CONTACT
+< /div >
+</body>
+//drop down menus
+
+
+
+</html>
